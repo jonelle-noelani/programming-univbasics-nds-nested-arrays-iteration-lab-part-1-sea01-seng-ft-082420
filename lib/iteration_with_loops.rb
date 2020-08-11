32db5f5ac794b6a50e366fa1a0ca86be75e651b1
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-11 09:32:16 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -21,8 +21,8 @@ end
 end
 # end
 
-# row_index = 0 
-# while row_index < src.count do
-#   element_index = 0 
-#   while element_index < src[row_index].count do
-#     w
+row_index = 0 
+while row_index < src.count do
+  element_index = 0 
+  while element_index < src[row_index].count do
+    w
